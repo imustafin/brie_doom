a87d89b94167600e666fc0e7282fe7c3e60a7166
Ilgiz Mustafin <ilgimustafin@gmail.com>
2021-09-05 19:14:46 +0300

    Initialize site

---
+++ site/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
